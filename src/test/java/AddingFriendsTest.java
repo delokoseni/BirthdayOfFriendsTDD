90610059eee91ad4cb0bd40fe04ce098ed568bdb
delokoseni <107359196+delokoseni@users.noreply.github.com>
2025-03-30 15:19:31 +0700

    Добавлен тест AddFriendTest для проверки метода AddFriend. Данный метод в классе AddingFriends пока что не реализован.

--- src/test/java/AddingFriendsTest.java
+++ src/test/java/AddingFriendsTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class AddingFriendsTest {
     @Test
@@ -19,6 +21,28 @@ public class AddingFriendsTest {
         addingFriends.CreateFriendList(FriendListName);
         File file = new File(FriendListName);
         Assertions.assertTrue(file.exists(), "Файл должен быть успешно создан.");
+    }
+
+    @Test
+    public void AddFriendTest() {
+        AddingFriends addingFriends = new AddingFriends();
+        String FriendListName = "FriendList.txt";
+        addingFriends.CreateFriendList(FriendListName);
+        String FriendName = "Иванов И.И.";
+        String FriendBirthday = "01.01.2001";
+        addingFriends.AddFriend(FriendListName, FriendName, FriendBirthday);
+        File file = new File(FriendListName);
+        Assertions.assertTrue(file.exists(), "Файл должен быть успешно создан.");
+        try {
+            Scanner scanner = new Scanner(file);
+            String expectedContent = FriendName + " - " + FriendBirthday;
+            String actualContent = scanner.nextLine();
+            Assertions.assertEquals(expectedContent, actualContent,
+                    "Содержимое файла должно соответствовать ожидаемому: Иванов И.И. - 01.01.2001");
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            Assertions.fail("Файл не найден");
+        }
     }
 
     @AfterEach
