fa95dfed2dac73713df02d4aae77c749ccc42204
delokoseni <107359196+delokoseni@users.noreply.github.com>
2025-03-30 14:51:35 +0700

    Добавлен метод CreateFriendList для класса AddingFriends. Тест CreateFriendListTest зеленый.

--- src/main/java/org/example/AddingFriends.java
+++ src/main/java/org/example/AddingFriends.java
@@ -1,5 +1,17 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+
 public class AddingFriends {
     public AddingFriends() { }
+
+    public void CreateFriendList(String FriendListName) {
+        try {
+            File file = new File(FriendListName);
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Ошибка при создании файла: " + e.getMessage());
+        }
+    }
 }
