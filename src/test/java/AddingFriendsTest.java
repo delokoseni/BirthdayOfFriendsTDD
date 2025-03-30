e7c9aa5ead23089b4fa659a449647ed0a42522af
delokoseni <107359196+delokoseni@users.noreply.github.com>
2025-03-30 14:29:57 +0700

    Добавлен тест CreateFriendListTest. Метод CreateFriendList в классе AddingFriends пока что не реализован.

--- src/test/java/AddingFriendsTest.java
+++ src/test/java/AddingFriendsTest.java
@@ -2,10 +2,21 @@ import org.example.AddingFriends;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 public class AddingFriendsTest {
     @Test
     public void AddingFriendsClassCreationTest() {
         AddingFriends addingFriends = new AddingFriends();
-        Assertions.assertNotNull(addingFriends);
+        Assertions.assertNotNull(addingFriends, "Экземпляр класса должен быть успешно создан.");
+    }
+
+    @Test
+    public void CreateFriendListTest() {
+        AddingFriends addingFriends = new AddingFriends();
+        String FriendListName = "FriendList.txt";
+        addingFriends.CreateFriendList(FriendListName);
+        File file = new File(FriendListName);
+        Assertions.assertTrue(file.exists(), "Файл должен быть успешно создан.");
     }
 }
