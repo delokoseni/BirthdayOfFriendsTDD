e28c6cd2eeeb2c93285156bfd5f5c5a783ff4234
delokoseni <107359196+delokoseni@users.noreply.github.com>
2025-03-30 16:09:36 +0700

    Добавил тест RemindCurrentDayTest для проверки метода RemindCurrentDay. Данный метод пока не реализован.

--- src/test/java/AddingFriendsTest.java
+++ src/test/java/AddingFriendsTest.java
@@ -59,6 +59,20 @@ public class AddingFriendsTest {
                 "Имя и дата рождения должны быть Иванов И.И. - 01.01.2001");
     }
 
+    @Test
+    public void RemindCurrentDayTest() {
+        AddingFriends addingFriends = new AddingFriends();
+        String FriendListName = "FriendList.txt";
+        addingFriends.CreateFriendList(FriendListName);
+        String FriendName = "Иванов И.И.";
+        String FriendBirthday = "01.01.2001";
+        addingFriends.AddFriend(FriendListName, FriendName, FriendBirthday);
+        int CurrentDay = 0;
+        Assertions.assertEquals("Иванов И.И. - 01.01.2001",
+                addingFriends.RemindCurrentDay(FriendListName, CurrentDay),
+                "Имя и дата рождения должны быть Иванов И.И. - 01.01.2001");
+    }
+
     @AfterEach
     public void Clean() {
         String FriendListName = "FriendList.txt";
