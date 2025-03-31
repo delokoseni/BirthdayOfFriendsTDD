54f2e0e11318662bf9fe0dccd80c93f4ff3b903b
delokoseni <107359196+delokoseni@users.noreply.github.com>
2025-03-31 11:49:49 +0700

    Добавил реализацию метода DeleteFriend. Тест DeleteFriendTest зеленый.

--- src/main/java/org/example/AddingFriends.java
+++ src/main/java/org/example/AddingFriends.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
 
 public class AddingFriends {
     public AddingFriends() { }
@@ -49,5 +52,36 @@ public class AddingFriends {
     public String GetNextBirthday(String FriendListName) {
         //todo реализовать метод GetNextBirthdayTest позднее до конца
         return "Иванов И.И. - 01.01.2001";
+    }
+
+    public void DeleteFriend(String FriendListName, String Friend) {
+        File file = new File(FriendListName);
+        File tempFile = new File(FriendListName + ".tmp");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            PrintWriter writer = new PrintWriter(new FileWriter(tempFile));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (!line.equals(Friend)) {
+                    writer.println(line);
+                } else {
+                    break;
+                }
+            }
+            while ((line = reader.readLine()) != null) {
+                writer.println(line);
+            }
+            reader.close();
+            writer.close();
+        }
+        catch (IOException eInside) {
+            System.out.println("Ошибка при чтении или записи файла: " + eInside.getMessage());
+        }
+        if (!file.delete()) {
+            System.out.println("Не удалось удалить исходный файл.");
+        }
+        if (!tempFile.renameTo(file)) {
+            System.out.println("Не удалось переименовать временный файл.");
+        }
     }
 }
